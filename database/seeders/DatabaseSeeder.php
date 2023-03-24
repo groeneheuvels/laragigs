0c9e6e76241bdcb6cf46cd5baacc35ac289ed253
Tosca van Schaik <toscavanschaik@gmail.com>
2023-03-24 16:17:58 +0100

    Create ListingFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,24 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        Listing::create([
-            'title' => 'laravel Senior Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'email@email.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor sit etc etc etc'
-        ]);
+        Listing::factory(6)->create();
 
-        Listing::create([
-            'title' => 'Full-Stack Engineer',
-            'tags' => 'laravel, backend, api',
-            'company' => 'Stark Industries',
-            'location' => 'New York, NY',
-            'email' => 'email2@email.com',
-            'website' => 'https://www.starkindustries.com',
-            'description' => 'Lorem ipsum dolor sit etc etc etc'
+        /* Listing::create([
+        'title' => 'laravel Senior Developer',
+        'tags' => 'laravel, javascript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston, MA',
+        'email' => 'email@email.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum dolor sit etc etc etc'
         ]);
+        Listing::create([
+        'title' => 'Full-Stack Engineer',
+        'tags' => 'laravel, backend, api',
+        'company' => 'Stark Industries',
+        'location' => 'New York, NY',
+        'email' => 'email2@email.com',
+        'website' => 'https://www.starkindustries.com',
+        'description' => 'Lorem ipsum dolor sit etc etc etc'
+        ]);*/
     }
 }
