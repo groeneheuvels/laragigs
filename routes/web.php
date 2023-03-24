5a5f952d2197c739f41f6ada4ebb5ad841818c5a
Tosca van Schaik <toscavanschaik@gmail.com>
2023-03-24 12:12:35 +0100

    First commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+/*Route::get('/hello', function () {
+return response('<h1>Hello World</h1>');
+});
+Route::get('/posts/{id}', function ($id) {
+// ddd($id);
+return response('Post' . $id);
+})->where('id', '[0-9]+');
+Route::get('/search', function (Request $request) {
+// dd($request->name . ' ' . $request->city);
+return $request->name . ' ' . $request->city;
+});*/
