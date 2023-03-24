439cbd4d66db92c23e169dae79421aba842528ad
Tosca van Schaik <toscavanschaik@gmail.com>
2023-03-24 12:35:15 +0100

    Add listings

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -6,7 +6,7 @@
 @endif
 @foreach ($listings as $listing)
     <h2>
-        {{$listing['title']}}
+       <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a> 
     </h2>
     <p>
         {{$listing['description']}}
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,18 @@ use App\Models\Listing;
 |
 */
 
+// All listings
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'Latest Listings',
         'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
     ]);
 });
 
