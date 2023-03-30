8d3d14714b30da1a77b62c867099d82fc8770e8f
Tosca van Schaik <toscavanschaik@gmail.com>
2023-03-30 10:17:40 +0200

    Fix problem with relationship  user - listing

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Listing extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'logo', 'company', 'location', 'website', 'email', 'tags', 'description'];
+    protected $fillable = ['title', 'logo', 'company', 'location', 'website', 'email', 'tags', 'description', 'user_id'];
 
     public function scopeFilter($query, array $filters)
     {
