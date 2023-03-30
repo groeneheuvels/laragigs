41ac8b147f317557bffa360c0c9e6fe6928db323
Tosca van Schaik <toscavanschaik@gmail.com>
2023-03-30 11:12:24 +0200

    Implement user authentication

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -66,6 +66,11 @@ class ListingController extends Controller
 
     public function update(Request $request, Listing $listing)
     {
+        // Make sure logged in user is owner
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $formFields = $request->validate([
             'title' => 'required',
             'company' => ['required'],
@@ -88,6 +93,10 @@ class ListingController extends Controller
     // Delete Listing
     public function destroy(Listing $listing)
     {
+        // Make sure logged in user is owner
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
         $listing->delete();
         return redirect('/')->with('message', 'Listing deleted successfully');
     }
